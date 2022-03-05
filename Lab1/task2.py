ada9ed143b2527a8f0b2818fac1b09860cd7e2d1
Artur <urzedowski.artur@gmail.com>
2022-03-05 12:29:40 +0100

    bugs fixed, finised task 1 and 2

--- Lab1/task2.py
+++ Lab1/task2.py
@@ -1,32 +1,43 @@
 # Random walkers
 
 import random
+import matplotlib.pyplot as plt
 
 direction = [-1, 1]
 
 d = 10
 
-kate_pos = 0 # Kate position
-john_pos = d # John pos
+kate_pos = [0] # Kate position
+john_pos = [d] # John pos
 
-time = 0
-while kate_pos != john_pos:
-    # Update positions
-    kate_update = random.choice(direction)
-    john_update = random.choice(direction)
+N = 4
+for x in range(1, N+1):
+    time = 0
+    while kate_pos[-1] != john_pos[-1]:
+        # Update positions
+        kate_update = random.choice(direction)
+        john_update = random.choice(direction)
+            
+        if (kate_pos[-1] + kate_update) < 0 or (kate_pos[-1] + kate_update) > d:
+            kate_pos.append(kate_pos[-1])
+        else:
+            kate_pos.append(kate_pos[-1] + kate_update)
+            
+        if (john_pos[-1] + john_update) < 0 or (john_pos[-1] + john_update) > d:
+            john_pos.append(john_pos[-1])
+        else:
+            john_pos.append(john_pos[-1] + john_update)
         
-    if (kate_pos + kate_update) < 0 or (kate_pos + kate_update) > d:
-        continue
-    else:
-        kate_pos += kate_update
-        
-    if (john_pos + john_update) < 0 or (john_pos + john_update) > d:
-        continue
-    else:
-        john_pos += john_update
+        print(kate_pos[-1], john_pos[-1])
+        time += 1
+
+    plt.subplot(2,2,x)
+    plt.plot(kate_pos,'-o', markersize=4)
+    plt.plot(john_pos,'-o', markersize=4)
+    plt.plot([0, len(kate_pos)-1], [kate_pos[-1], kate_pos[-1]], color='red', marker='o')
+    plt.title(f"Met after {time}")
+    kate_pos = [0] # Kate position
+    john_pos = [d] # John pos
     
-    print(kate_pos, john_pos)
-    time += 1
-
-print(f"met after {time}")
+plt.show()
 
